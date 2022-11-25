8b06118ce37bf718b111a80b627e0b5b8fb7c80a
Ricardo <ricoazeredo@yahoo.com.br>
2022-11-25 00:24:51 -0300

    update edit product image

---
+++ recebedor.php
@@ -0,0 +1,24 @@
+<?php
+session_start();
+ob_start();
+require 'config.php';
+$id = $_SESSION['id'];
+
+$sql = $pdo->query("SELECT * FROM produtos WHERE id = '$id'");
+
+$permitidos = array('image/jpg','image/jpeg','image/png');
+
+ if(in_array($_FILES['img']['type'], $permitidos)){
+    $imagem = $_FILES['img']['name'];
+    move_uploaded_file($_FILES['img']['tmp_name'], 'assets/img/'.$imagem);
+
+      $sql = $pdo->prepare("UPDATE produtos SET imagem_url = :img WHERE id = :id");
+      $sql->bindValue(':id',$id);
+      $sql->bindValue(':img',$imagem);
+      $sql->execute();
+
+    echo 'Arquivo salvo com sucesso';
+    header('Location: editar-produto.php');
+ } else {
+    echo 'Arquivo não permitido';
+ }
